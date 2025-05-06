83d3cdbf5f15b81e880ec15d31e7d5327b491192
Zakaria <zakiboutarfa@gmail.com>
2025-05-06 14:40:15 +0000

    followed the practical 6 handout (created test and class)

---
+++ Testing1/tstStock.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsAlbum  anAlbum = new clsAlbum();
+            Assert.IsNotNull( anAlbum);
+        }
+    }
+}
